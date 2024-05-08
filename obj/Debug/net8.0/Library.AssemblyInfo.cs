c6dd9cfee8705ef3dede63d10275e73a8471b037
Magdalena <magdalenamagde2703@gmail.com>
2024-05-09 01:20:30 +0200

    commit

--- obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854ce8e2fd53c8744f9c5390dda6fa8d41277493")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
